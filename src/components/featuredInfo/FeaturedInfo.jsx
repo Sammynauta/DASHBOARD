27e143e7dc9f0701eeb6454b2835f5860405d102
Sammynauta <samuka.o.diogo@gmail.com>
2021-11-09 13:46:16 -0300

    criando painel de info.

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,33 @@
+import "./FeaturedInfo.css";
+import {ArrowDownward, ArrowUpward} from "@material-ui/icons";
+
+export default function FeaturedInfo() {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <span className="featuredTitle">Revanue</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,225</span>
+                    <span className="featuredMoneyRate">-11.58 <ArrowDownward /> </span>
+                </div>
+                <span className="featuredSub">Compared to last month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Sales</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$5,225</span>
+                    <span className="featuredMoneyRate">-1.58 <ArrowDownward /> </span>
+                </div>
+                <span className="featuredSub">Compared to last month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Cost</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,853</span>
+                    <span className="featuredMoneyRate"> +3.8 <ArrowUpward /> </span>
+                </div>
+                <span className="featuredSub">Compared to last month</span>
+            </div>
+        </div>
+    )
+}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./sidebar.css";
-import {LineStyle,Timeline,TrendingUp} from "@material-ui/icons";
+import {LineStyle,Timeline,TrendingUp, PeopleAlt, ShoppingCart,Equalizer , AttachMoney,Report, Email, DynamicFeed, Chat, LocalMall} from "@material-ui/icons";
 
 export default function Sidebar() {
     return (
@@ -19,6 +19,61 @@ export default function Sidebar() {
                         <li className="sidebarListItem">
                             <TrendingUp className="sidebarIcon" />
                             Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Quick Menu</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <PeopleAlt className="sidebarIcon" />
+                            Users
+                        </li>
+                        <li className="sidebarListItem">
+                            <ShoppingCart className="sidebarIcon" />
+                            Products
+                        </li>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon" />
+                            Transactions
+                        </li>
+                        <li className="sidebarListItem">
+                            <Equalizer className="sidebarIcon" />
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Notifications</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <Email className="sidebarIcon" />
+                            Mail
+                        </li>
+                        <li className="sidebarListItem">
+                            <DynamicFeed className="sidebarIcon" />
+                            Feedbacks
+                        </li>
+                        <li className="sidebarListItem">
+                            <Chat className="sidebarIcon" />
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Staff</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <LocalMall className="sidebarIcon" />
+                            Manage
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon" />
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <Report className="sidebarIcon" />
+                            Reports
                         </li>
                     </ul>
                 </div>
