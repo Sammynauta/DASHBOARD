4c4c50540caf01b524466caa10bdd2ed85881129
Sammynauta <samuka.o.diogo@gmail.com>
2021-11-18 16:57:58 -0300

    pagina de novo usuario em processo.

--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -1,9 +1,53 @@
 import "./NewUser.css";
 
-export default function newUser() {
+export default function NewUser() {
     return (
         <div className="newUser">
-            new user page
+          <h1 className="newUserTitle">New User</h1>
+          <form action="" className="newUserForm">
+              <div className="newUserItem">
+                  <label>Username</label>
+                  <input type="text" placeholder='john' />
+              </div>
+              <div className="newUserItem">
+                  <label>Full Name</label>
+                  <input type="text" placeholder='john Smith' />
+              </div>
+              <div className="newUserItem">
+                  <label>Email</label>
+                  <input type="email" placeholder='john@email.com' />
+              </div>
+              <div className="newUserItem">
+                  <label>Password</label>
+                  <input type="password" placeholder='password' />
+              </div>
+              <div className="newUserItem">
+                  <label>Phone</label>
+                  <input type="text" placeholder='+11 123 456 789' />
+              </div>
+              <div className="newUserItem">
+                  <label>Address</label>
+                  <input type="text" placeholder='Rio de Janeiro | BRA' />
+              </div>
+              <div className="newUserItem">
+                  <label>Gender</label>
+                    <div className="newUserGender">
+                    <input type="radio" name='gender' id='male' value='male'/>
+                    <label for="male">Male</label>
+                    <input type="radio" name='gender' id='female' value='female'/>
+                    <label for="female">Female</label>
+                    <input type="radio" name='gender' id='others' value='others'/>
+                    <label for="others">Others</label>
+                  </div>
+              </div>
+              <div className="newUserItem">
+                  <label >Active</label>
+                  <select className='newUserSelect' name="active" id="active">
+                      <option value="yes">Yes</option>
+                      <option value="no">No</option>
+                  </select>
+              </div>
+          </form>
         </div>
     )
 }
