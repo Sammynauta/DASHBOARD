16f117d61e42145e1e6f34785d5658e0703a32f6
Sammynauta <samuka.o.diogo@gmail.com>
2021-11-19 13:06:28 -0300

    pagina de create novo usuario concluida.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import "./sidebar.css";
 import {LineStyle,Timeline,TrendingUp, PeopleAlt, ShoppingCart,Equalizer , AttachMoney,Report, Email, DynamicFeed, Chat, LocalMall} from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
     return (
@@ -8,10 +9,12 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
+                        <Link to="/" className='link'>
                         <li className="sidebarListItem active">
                             <LineStyle className="sidebarIcon" />
                             Home
                         </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <Timeline className="sidebarIcon" />
                             Analytics
@@ -25,14 +28,18 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
+                        <Link to="/users" className='link'>
                         <li className="sidebarListItem">
                             <PeopleAlt className="sidebarIcon" />
                             Users
                         </li>
+                        </Link>
+                        <Link to="/products" className='link'>
                         <li className="sidebarListItem">
                             <ShoppingCart className="sidebarIcon" />
                             Products
                         </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <AttachMoney className="sidebarIcon" />
                             Transactions
